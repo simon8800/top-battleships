e26b13b6cb1bb9eec8eeecc5c264e4a7b6d90893
Simon Mei <smei8800@gmail.com>
2024-06-26 13:21:14 -0400

    Create Ship class

---
+++ src/modules/Ship.js
@@ -0,0 +1,16 @@
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+    this.sunk = false;
+    this.hits = 0;
+  }
+
+  hit() {
+    this.hits += 1;
+  }
+
+  isSunk() {
+    return this.hits === this.length;
+  }
+}
